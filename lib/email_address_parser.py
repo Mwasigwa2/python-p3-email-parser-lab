2b315514f90848b75b97bbbbc6b6536162a2cc12
Mwasigwa2 <gathirimu.mwasigwa@student.moringaschool.com>
2023-09-13 11:11:24 +0300

    errors fixed

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,14 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        # Split the email addresses using a regular expression
+        addresses = re.split(r'[,\s]+', self.email_addresses)
+        # Remove empty strings and duplicate email addresses
+        unique_addresses = list(set(filter(lambda x: x != '', addresses)))
+        # Sort the addresses alphabetically
+        unique_addresses.sort()
+        return unique_addresses
